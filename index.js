f64ce65960f58c4cb5c543f20d157e986a7dcaa3
Cynthiaaira <cynthiakaira02@gmail.com>
2026-03-29 15:27:26 +0300

    Added node version

--- index.js
+++ index.js
@@ -159,4 +159,4 @@ app.post('/auth/login', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`CHUMS API running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0' ,() => console.log(`CHUMS API running on port ${PORT}`));
