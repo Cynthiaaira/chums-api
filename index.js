90c102a293bd7335a31f8f7770476895e400c484
Cynthiaaira <cynthiakaira02@gmail.com>
2026-04-02 04:22:03 +0300

    fresh build

--- index.js
+++ index.js
@@ -324,5 +324,6 @@ app.post('/auth/register', (req, res) => {
 );
 }); 
 /// fresh build
+/// fresh build
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0' ,() => console.log(`CHUMS API running on port ${PORT}`));
